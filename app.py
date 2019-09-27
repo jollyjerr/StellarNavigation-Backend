b30549260c45d6298f6e381c267cc47e768feb5f
jollyjerr <jollyjerr@gmail.com>
2019-09-27 08:29:02 -0600

    Functional GET and POST for stellar systems

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import logging
 import os
+
+# config debug
+logging.basicConfig(level=logging.DEBUG)
 
 # import models
 from models.StellarSystemModel import StellarSystem, StellarSystemSchema
@@ -8,23 +13,33 @@ from models.StellarSystemModel import StellarSystem, StellarSystemSchema
 stellar_system_schema = StellarSystemSchema()
 stellar_systems_schema = StellarSystemSchema(many=True)
 
+#import functional app
 from models import app, db, ma
+CORS(app)
 
 # routes
 @app.route('/stellarsystems', methods=['GET'])
 def get_stellar_systems():
     all_stellar_systems = StellarSystem.query.all()
-    result = stellar_system_schema.dump(all_stellar_systems)
-    return jsonify(result.data)
+    result = stellar_systems_schema.dump(all_stellar_systems)
+
+    return jsonify(result)
 
 @app.route('/stellarsystem', methods=['POST'])
 def add_stellar_system():
+    name = request.json['name']
+
+    new_stellar_system = StellarSystem(name)
+
+    db.session.add(new_stellar_system)
+    db.session.commit()
+
+    return stellar_system_schema.jsonify(new_stellar_system)
 
 
-    return jsonify({ "Hello": "World!"})
 
 
 
-# run.py
+# run
 if __name__ == '__main__':
     app.run(debug=True)
