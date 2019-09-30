573b883378e8d2d466478b41e74e1065ebd626df
jollyjerr <jollyjerr@gmail.com>
2019-09-30 08:35:15 -0600

    seeds large celestials in solar system

--- models/LargeCelestialModel.py
+++ models/LargeCelestialModel.py
@@ -11,21 +11,21 @@ class LargeCelestial(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
     classification = db.Column(db.String(10), nullable=False)
-    radius = db.Column(db.Float, nullable=False)
-    orbital_period = db.Column(db.Float, default=0)
-    semi_major_axis = db.Column(db.Float, default=0)
+    radius = db.Column(db.Float, nullable=False) #km
+    orbital_period = db.Column(db.Float, default=0) #days
+    semi_major_axis = db.Column(db.Float, default=0) #km - if possible
     stellar_system_id = db.Column(db.Integer, db.ForeignKey('stellar_system.id'), nullable=False)
     smallCelestials = db.relationship('SmallCelestial', backref='large_celestial', lazy=True)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
 
-    def __init__(self, data):
-        self.name = data.get('name')
-        self.classification = data.get('classification')
-        self.radius = data.get('radius')
-        self.orbital_period = data.get('orbital_period')
-        self.semi_major_axis = data.get('semi_major_axis')
-        self.stellar_system_id = data.get('stellar_system_id')
+    def __init__(self, name, classification, radius, orbital_period, semi_major_axis, stellar_system_id):
+        self.name = name
+        self.classification = classification
+        self.radius = radius
+        self.orbital_period = orbital_period
+        self.semi_major_axis = semi_major_axis
+        self.stellar_system_id = stellar_system_id
         self.created_at = datetime.datetime.utcnow()
         self.modified_at = datetime.datetime.utcnow()
 
--- seeds.py
+++ seeds.py
@@ -16,14 +16,25 @@ from models.StellarSystemModel import StellarSystem, StellarSystemSchema
 #Seeds
 @manager.command
 def seed():
-    test_stellar_system = StellarSystem.query.filter_by(name="Bomba").first().id
-    test_small = SmallCelestial("Little Fella", test_stellar_system)
-    test_large = LargeCelestial("Big Fella", test_stellar_system)
+    # solar_system = StellarSystem(name="Solar System")
+    solar_system = StellarSystem.query.filter_by(name="Solar System").first().id
 
-    # db.session.add(test_stellar_system)
-    db.session.add(test_small)
-    db.session.add(test_large)
-    # db.session.add_all([])
+    # sun = LargeCelestial(name="Sun", classification="body", radius=695510.0, orbital_period=82125000000.0, semi_major_axis=000.0, stellar_system=solar_system)   
+    # mercury = LargeCelestial(name="Mercury", classification="body", radius=2439.7, orbital_period=88.0, semi_major_axis=57909050.0, stellar_system=solar_system)
+    # venus = LargeCelestial(name="Venus", classification="body", radius=6051.8, orbital_period=255.0, semi_major_axis=108208000.0, stellar_system=solar_system)
+    # earth = LargeCelestial(name="Earth", classification="body", radius=6356.0, orbital_period=365.0, semi_major_axis=149598023.0, stellar_system=solar_system)
+    # mars = LargeCelestial(name="Mars", classification="body", radius=3389.5, orbital_period=687.0, semi_major_axis=227943771.6, stellar_system=solar_system)
+    # jupiter = LargeCelestial(name="Jupiter", classification="body", radius=69911.0, orbital_period=4380.0, semi_major_axis=778570000.0, stellar_system=solar_system)
+    # saturn = LargeCelestial(name="Saturn", classification="body", radius=58232.0, orbital_period=10585.0, semi_major_axis=1433530000.0, stellar_system=solar_system)
+    # uranus = LargeCelestial(name="Uranus", classification="body", radius=25362.0, orbital_period=30660.0, semi_major_axis=2875031718.261, stellar_system=solar_system)
+    # neptune = LargeCelestial(name="Neptune", classification="body", radius=24622.0, orbital_period=60225.0, semi_major_axis=4504390009.0, stellar_system=solar_system)
+
+    # db.session.add(solar_system)
+    # db.session.add_all([sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune])
+
+    
+
+
     db.session.commit()
 
 
